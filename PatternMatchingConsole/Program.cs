a3c9a4b84d5e477c1b38b37cc8d98fde2ad63316
tany29 <yew_hsiung.tan@contractores.roche.com>
2018-06-08 22:52:08 +0800

    add simple command line interface

--- PatternMatchingConsole/Program.cs
+++ PatternMatchingConsole/Program.cs
@@ -6,7 +6,32 @@ namespace PatternMatchingConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Pattern Matching Test Console");
+
+            while (true)
+            {
+                Console.WriteLine();
+                Console.Write("Input (blank to exit): ");
+                var input = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(input))
+                {
+                    break;
+                }
+
+                Console.Write("Pattern (blank to exit): ");
+                var pattern = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(pattern))
+                {
+                    break;
+                }
+
+                Console.WriteLine("Result: {0}", PatternMatcher.IsMatch(input, pattern) ? "Matched" : "Not Matched");
+            }
+
+            Console.WriteLine("Bye");
+            Console.Read();
         }
     }
 }
